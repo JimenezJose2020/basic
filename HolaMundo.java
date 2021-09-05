4aea4c1dc231f405e4fdfa7726968fae39454c61
JimenezJose2020 <90002736+JimenezJose2020@users.noreply.github.com>
2021-09-05 14:15:29 -0500

    hotfix

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,11 @@
 public class HolaMundo {
     public static void main(String[] args) {		
+		for (int  i = 0; i <= 3; i++){
+		
 		System.out.println("Hola Mundo");		
 		System.out.println("Buenas tardes");
-	}
-
+	}}
+ 
 }
 
 
